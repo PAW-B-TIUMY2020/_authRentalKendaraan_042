927e87c92c124936d97714963b9212614ce2467d
Lingga Buwono <lingga.buwono.ft18@mail.umy.ac.id>
2020-12-07 19:04:07 +0700

    Add project files.

---
+++ RentalKendaraan/Models/Peminjaman.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentalKendaraan_042.Models
+{
+    public partial class Peminjaman
+    {
+        public Peminjaman()
+        {
+            Pengembalian = new HashSet<Pengembalian>();
+        }
+
+        [DisplayName("ID")]
+        public int IdPeminjaman { get; set; }
+        [Required(ErrorMessage = "Tanggal peminjaman tidak boleh kosong!")]
+        public DateTime? TglPeminjaman { get; set; }
+        [DisplayName("Kendaraan")]
+        public int? IdKendaraan { get; set; }
+        [DisplayName("Costumer")]
+        public int? IdCustomer { get; set; }
+        [DisplayName("Jaminan")]
+        public int? IdJaminan { get; set; }
+        public int? Biaya { get; set; }
+
+
+        [DisplayName("Customer")]
+        public Customer IdCustomerNavigation { get; set; }
+        [DisplayName("Jaminan")]
+        public Jaminan IdJaminanNavigation { get; set; }
+        [DisplayName("Kendaraan")]
+        public Kendaraan IdKendaraanNavigation { get; set; }
+        public ICollection<Pengembalian> Pengembalian { get; set; }
+    }
+}
